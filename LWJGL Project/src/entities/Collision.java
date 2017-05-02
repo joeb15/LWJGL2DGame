b4096c35333c6781f87f64c3f3c9a82936476be0
Joe Banko <joebanko15@gmail.com>
2017-05-01 21:29:02 -0700

    Simplified Collision

--- LWJGL Project/src/entities/Collision.java
+++ LWJGL Project/src/entities/Collision.java
@@ -2,23 +2,17 @@
 
 package entities;
 
-import org.joml.Vector2f
+import org.joml.Vector2f;
 
 public class Collision {
 
 	public static boolean collision(Entity a, Entity b) {
-		double maxXA = a.getPos().x + (a.getSize().x / 2);
-		double minXA = maxXA - a.getSize().x;
-		double maxXB = b.getPos().x + (b.getSize().x / 2);
-		double minXB - maxYB - b.getSize().x;
-
-		double maxYA = a.getPos().y + (a.getSize().y / 2);
-		double minYA = maxYA - a.getSize().y;
-		double maxYB = b.getPos().y + (b.getSize().y / 2);
-		double minYB - maxYB - b.getSize().y;
-
-		return (minXA < maxXB && maxXA > minXB && minYA > maxYB && maxYA < minYB)
-	}
+        Vector2f aS = a.getSize();
+        Vector2f aP = a.getPos();
+        Vector2f bS = b.getSize();
+        Vector2f bP = b.getPos();
+        return (Math.abs(aP.x - bP.x) < aS.x + bS.x) && (Math.abs(aP.y - bP.y) < aS.y + bS.y);
+    }
 }
 
 
